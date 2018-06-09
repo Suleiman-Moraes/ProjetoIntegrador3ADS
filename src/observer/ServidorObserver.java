3699d9f00add502a0512b2b65617ae5027ae213a
Suleiman Moraes <30638390+Suleiman-Moraes@users.noreply.github.com>
2018-06-09 12:29:09 -0300

    ServidorObserver

---
+++ src/observer/ServidorObserver.java
@@ -0,0 +1,28 @@
+package observer;
+
+import interfaces.IObservador;
+import interfaces.IServidorObserver;
+
+public class ServidorObserver implements IServidorObserver{
+
+    @Override
+    public void incluirNaRede(IObservador cv) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void retirarDaRede(IObservador cv) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void dispararInformacoes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setInformacao(IObservador cv) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
